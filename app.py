07e1402953faa4dcfd07e6618886e4acb425b3b3
lorenzochahid <chahid.lorenzo@protonmail.com>
2020-06-15 10:59:41 +0200

    first test for route

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World toi"
+    return "Hello World"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':
